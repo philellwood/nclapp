33ccd8a752561add40cc6a132be7fdd39e907598
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-15 00:24:49 +0100

    Updated to use the latest Util functions and with Phil's event listener.

--- Resources/windows/societies.js
+++ Resources/windows/societies.js
@@ -1,4 +1,15 @@
 Ti.include('/util.js');
 Ti.include('/clubs.js');
 
-Ti.UI.currentWindow.add(Util.createSimpleDataTable(clubs));
+var table;
+
+table = Util.createSimpleDataTable(clubs, {
+  'table': { },
+  'row': { }
+});
+
+table.addEventListener('click', function (e) {
+  e.rowData.backgroundColor = '#0f0';
+});
+
+Ti.UI.currentWindow.add(table);
