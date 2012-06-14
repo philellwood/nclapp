f15bbd0f9c8e164949224f92284efe2c9163f029
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-14 23:56:14 +0100

    Added namespace Util. Modified table creation function to be less specific. Added tab-group builder function.

--- Resources/util.js
+++ Resources/util.js
@@ -1,12 +1,39 @@
-Ti.include('clubs.js');
+var Util = {};
 
-function createClubTable(){
-	var tableData = [];
-    for (var i = 0; i<clubs.length; i++){
-    	tableData.push({title : clubs[i]});
-    }
-	var table = Ti.UI.createTableView({
- 		 data: tableData
-	});
-	return table;
-}
+Util.createSimpleDataTable = function (input) {
+  var tableData, i, len;
+  tableData = [];
+  for (i = 0, len = input.length; i < len; i += 1){
+    tableData.push({title : input[i]});
+  }
+  return Ti.UI.createTableView({
+    data: tableData
+  });;
+};
+
+Util.buildTabGroup = function (data) {
+  var index, length, datum, tabs, window;
+  
+  tabs = [];
+  for (index = 0, length = data.length;
+       index < length;
+       index += 1) {
+    datum = data[index];
+    
+    window = Ti.UI.createWindow({
+      'title': datum.title || 'No title',
+      'url': datum.url || '',
+      'backgroundColor': '#fff'
+    });
+    
+    tabs.push(Ti.UI.createTab({
+      'title': datum.title,
+      'icon': datum.icon,
+      'window': window
+    }));
+  }
+  
+  return Ti.UI.createTabGroup({
+    'tabs': tabs
+  });
+};
