1480939cbab1f92db662763949cf6e72c3752a25
phil <phil@phils-MacBook-Pro.local>
2012-06-19 21:39:22 +0100

    added functions to userhandler

--- Resources/userHandler.js
+++ Resources/userHandler.js
@@ -1,2 +1,68 @@
 var Cloud = require('ti.cloud');
 Cloud.debug = true;
+
+var Users = {};
+
+Users.create = function(_data){
+	Cloud.Users.create({
+    username: _data.username,
+    password: _data.password,
+    password_confirmation: _data.confirmPassword,
+    first_name: firstName.value,
+    last_name: lastName.value,
+    custom_fields: '{"clubs":""}' //arrays cant be queried, so store clubs as string
+}, function (e) {
+    if (e.success) {
+		// user created successfully
+    } else {
+        // oops, something went wrong
+    }
+});
+};
+
+Users.login = function(_data){
+	Cloud.Users.login({
+      login: _data.login,
+      password: _data.password
+    }, function (e) {
+      if (e.success) {
+        var user = e.users[0];
+        Ti.API.log('Success:\\n' +
+            'id: ' + user.id + '\\n' +
+            'first name: ' + user.first_name + '\\n' +
+            'last name: ' + user.last_name);
+      } else {
+        Ti.API.error('Error:\\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+      }
+    });
+	
+};
+
+Users.showCurrent = function(){
+	Cloud.Users.showMe(function (e) {
+      if (e.success) {
+        var user = e.users[0];
+        Ti.API.log(user);
+        return user;
+      } else {
+        Ti.API.error('Error:\\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+      }
+    });
+};
+
+Users.update = function(_data){
+  Cloud.Users.update(
+  	_data,
+  	function (e) {
+    if (e.success) {
+        var user = e.users[0];
+        Ti.API.log(user);
+    } else {
+        Ti.API.error('Error:\\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+    }
+  });
+	
+};
