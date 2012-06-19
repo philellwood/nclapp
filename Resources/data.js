7440176a7c7baba5f5ef3d336ee5e630e55c0b10
phil <phil@phils-MacBook-Pro.local>
2012-06-19 21:38:44 +0100

    added some cloud functions to data.js

--- Resources/data.js
+++ Resources/data.js
@@ -4,11 +4,28 @@ Data.CLUBS = 'clubs';
 
 Data.getUserClubs = function () {
 	if (!Ti.App.Properties.hasProperty(Data.CLUBS)){
-  	Data.setUserClubs({});
+  	  Data.setUserClubs({});
 	}
 	return JSON.parse(Ti.App.Properties.getString(Data.CLUBS));
 };
 
+Data.downloadClubs = function(){
+  var custom_fields = JSON.parse(Users.showCurrent.custom_fields);
+  Ti.API.debug(custom_fields);
+  var clubString = custom_fields.clubs;
+  return clubString.split(";");
+};
+
+Data.updateCloudClubs = function(){
+	var clubString; //get a string of clubs
+	Users.update({
+		custom_fields: '{"clubs":"'+clubString+'"}'
+	});
+};
+
+Data.clubsToString = function(){
+	//... .join(";");
+};
 Data.setUserClubs = function (clubs) {
   Ti.App.Properties.setString(Data.CLUBS, JSON.stringify(clubs));
 };
