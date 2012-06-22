0a67a25155eb6aa2088a5693275ecce7e193a574
phil <phil@phils-MacBook-Pro.local>
2012-06-22 21:58:49 +0100

    changed format of customfield.clubs

--- Resources/data.js
+++ Resources/data.js
@@ -17,17 +17,16 @@ Data.downloadClubs = function(){
 };
 
 Data.updateCloudClubs = function(){
-	var clubString; //get a string of clubs
+	
 	Users.update({
-		custom_fields: '{"clubs":"'+clubString+'"}'
+		custom_fields: '{"clubs":'+JSON.stringify(Data.getUserClubs())+'}'
 	});
 };
 
-Data.clubsToString = function(){
-	//... .join(";");
-};
+
 Data.setUserClubs = function (clubs) {
   Ti.App.Properties.setString(Data.CLUBS, JSON.stringify(clubs));
+  Data.updateCloudClubs();
 };
 
 Data.subscribeToClub = function(clubName) {
