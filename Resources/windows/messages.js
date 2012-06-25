0afd98a7045dc15e4e2e0bc9f018956b129352c5
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-25 23:19:00 +0100

    Added a 'new message' button that launches the new_message window.

--- Resources/windows/messages.js
+++ Resources/windows/messages.js
@@ -1,23 +1,39 @@
 Ti.include('/data.js');
+(function (window) {
+  var newMsgButton, deleteButton;
 
-Ti.UI.currentWindow.add(Ti.UI.createLabel({
-  text: 'Hello, world.',
-  top: 30,
-  height: 'auto',
-  width: 'auto'
-}));
+  Ti.UI.currentWindow.add(Ti.UI.createLabel({
+    text: 'Hello, world.',
+    top: 30,
+    height: 'auto',
+    width: 'auto'
+  }));
 
-var deleteButton = Ti.UI.createButton({
-   title: 'Unsubscribe from all Clubs',
-   top: 10,
-   width: 300,
-   height: 50
-});
+  newMsgButton = Ti.UI.createButton({
+     title: 'New Message',
+     top: 10,
+     width: 300,
+     height: 50
+  });
+  newMsgButton.addEventListener('click', function (e) {
+    if (Data.getUserClubs() !== {}) {
+      Ti.UI.createWindow({ url: 'new_message.js', modal: true }).open();
+    } else {
+      alert("You do not belong to any groups.");
+    }
+  });
 
-deleteButton.addEventListener('click',function(e)
-{
-	Data.removeAllClubs();
-	Titanium.API.info("Deleted clubs");
-});
+  deleteButton = Ti.UI.createButton({
+     title: 'Unsubscribe from all Clubs',
+     top: 70,
+     width: 300,
+     height: 50
+  });
+  deleteButton.addEventListener('click', function (e) {
+    Data.removeAllClubs();
+    Titanium.API.info("Deleted clubs");
+  });
 
-Ti.UI.currentWindow.add(deleteButton);
+  window.add(newMsgButton, deleteButton);
+
+}).call(Ti.UI.currentWindow, Ti.UI.currentWindow);
