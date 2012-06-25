89c521e9b2c2438966e47e8786f3eabf1df03fd8
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-25 23:19:31 +0100

    Defined the new_message window. Contains a cancel button, and send button, a club picker, and a message textarea.

---
+++ Resources/windows/new_message.js
@@ -0,0 +1,44 @@
+Ti.include('/util.js');
+Ti.include('/data.js');
+Ti.include('/clubs_data.js');
+(function (window) {
+  var cancel, send, message, clubPicker;
+  
+  window.updateLayout({
+    title: "New Message",
+    backgroundColor: '#fff'
+  });
+  
+  cancel = (window.leftNavButton = Ti.UI.createButton({ title: "Cancel" }));
+  cancel.addEventListener('click', function () {
+    window.close();
+  });
+  
+  send = (window.rightNavButton = Ti.UI.createButton({ title: "Send" }));
+  send.addEventListener('click', function () {
+    // Messages.create({subject,body,...});
+    alert("This will send...");
+    window.close();
+  });
+  
+  message = Ti.UI.createTextArea({
+    top: 5, bottom: 222,
+    left: 5, right: 5,
+    borderWidth: 1, borderColor: '#bbb', borderRadius: 3
+  });
+  window.add(message);
+  
+  clubPicker = Ti.UI.createPicker({ bottom: 0 });
+  window.add(clubPicker);
+  clubPicker.add((function () {
+    var userclubs = Data.getUserClubs(), result = [];
+    Util.foreach(clubsData, function (_, title) {
+      if (title in userclubs) {
+        result.push(Ti.UI.createPickerRow({ title: title }));
+      }
+    });
+    return result;
+  })());
+  clubPicker.selectionIndicator = true;
+  
+}).call(Ti.UI.currentWindow, Ti.UI.currentWindow);
