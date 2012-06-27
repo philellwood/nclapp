e154abb3667aba9c19e8da1b4095a53f6edf2f81
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-27 20:32:27 +0100

    Reconstructed for the app window manager and events system.

--- Resources/windows/main.js
+++ Resources/windows/main.js
@@ -1,28 +1,48 @@
 Ti.include('/data.js');
 Ti.include('/util.js');
 
-var showMain = function () {
-  
+var createMainWindow = function () {
   Data.getCloudClubs();
   
-  Util.buildTabGroup([
+  var group = Ti.UI.createTabGroup({
+    tabs: Util.foreach([
+      { 'title': "Messages",
+        'url': "windows/messages.js",
+        'icon': "/images/08-chat.png" },
 
-    { 'title': "Messages",
-      'url': "windows/messages.js",
-      'icon': "/images/08-chat.png" },
+      { 'title': "Events",
+        'url': "windows/events.js",
+        'icon': "/images/88-beer-mug.png" },
 
-    { 'title': "Events",
-      'url': "windows/events.js",
-      'icon': "/images/88-beer-mug.png" },
+      { 'title': "Clubs",
+        'url': "windows/clubs.js",
+        'icon': "/images/112-group.png" },
 
-    { 'title': "Clubs",
-      'url': "windows/clubs.js",
-      'icon': "/images/112-group.png" },
-    
-    {
-    	'title': "Settings",
-    	'url': "windows/settings.js",
-    	'icon': "/images/157-wrench.png"}
+      { 'title': "Settings",
+      	'url': "windows/settings.js",
+      	'icon': "/images/157-wrench.png" }
 
-  ]).open();
+    ], function (_, item) {
+      var window;
+      
+      window = Ti.UI.createWindow({
+        'title': item.title || 'No title',
+        'url': item.url || '',
+        'backgroundColor': '#fff',
+        'barColor': Util.theme.mainColor
+      });
+      
+      window.addEventListener('logout', function () {
+        group.fireEvent('logout');
+      });
+
+      return Ti.UI.createTab({
+        'title': item.title,
+        'icon': item.icon,
+        'window': window
+      });
+    })
+  });
+  
+  return group;
 };
