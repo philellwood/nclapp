604cba45e48d70e1119fec4f85d75d0e4ed6a3b9
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-27 20:34:44 +0100

    No-longer has login controls. Has a logout button that uses the window manager events system.

--- Resources/windows/settings.js
+++ Resources/windows/settings.js
@@ -1,56 +1,14 @@
-Ti.include('/userHandler.js');
-var username = Ti.UI.createTextField({
-  borderWidth:2,
-  borderColor:'#000',
-  top:10,
-  width: 300,
-  height: 50
-});
-var password = Ti.UI.createTextField({
-  borderWidth:2,
-  borderColor:'#000',
-  top:70,
-  width: 300,
-  height: 50,
-  passwordMask: true
-});
+Ti.include('/util.js');
 
-var createButton = Ti.UI.createButton({
-  width: 300,
-  height: 50,
-  title: 'create'
-	
-});
-
-createButton.addEventListener('click',function(e){
-	Users.create({
-		username: username.getValue(),
-		password: password.getValue(),
-		confirmPassword: password.getValue()
-	});
-	
-});
-var loginButton = Ti.UI.createButton({
-	top:200,
-  width: 300,
-  height: 50,
-  title: 'login'
-	
-});
-
-loginButton.addEventListener('click',function(e){
-	Data.save("login", username.getValue());
-	Data.save("password", password.getValue());
-	Users.login({
-		login: Data.load("login"),
-		password: Data.load("password")
-		
-	}, function(){
-		alert("successfully logged in");
-	});
-	
-});
-Ti.currentWindow.add(username);
-Ti.currentWindow.add(password);
-Ti.currentWindow.add(createButton);
-Ti.currentWindow.add(loginButton);
+(function (window) {
+  
+  var logout;
+  
+  logout = (window.rightNavButton = Ti.UI.createButton({
+    title: 'Logout'
+  }));
+  logout.addEventListener('click', function () {
+    window.fireEvent('logout');
+  });
+  
+}).call(Ti.UI.currentWindow, Ti.UI.currentWindow);
