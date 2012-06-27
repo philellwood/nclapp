1bcf8d0364224625652b0338ec1ab577f8f60e6d
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-27 11:45:45 +0100

    Transformed data sending to event system.

--- Resources/app.js
+++ Resources/app.js
@@ -4,7 +4,10 @@ Ti.include('/windows/main.js');
 
 Ti.UI.setBackgroundColor('#000');
 
-Ti.UI.createWindow({
-  url: '/windows/login.js',
-  success: showMain
-}).open();
+var loginWindow = Ti.UI.createWindow({
+  url: '/windows/login.js'
+});
+loginWindow.addEventListener('open', function () {
+  loginWindow.fireEvent('setSuccess', { success: showMain });
+});
+loginWindow.open();
