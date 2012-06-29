e38a93cf59454d464199a341097c68a4c4c5e35a
Liam Goodacre <goodacre.liam@gmail.com>
2012-06-29 21:34:25 +0100

    Refresh button, bottom toolbar. Button title doesn't update? Strange.

--- Resources/windows/events.js
+++ Resources/windows/events.js
@@ -2,21 +2,115 @@ Ti.include('/util.js');
 Ti.include('/data.js');
 Ti.include('/eventHandler.js');
 (function (window, tab) {
-  var createEvent, settings, clubLabel, club, requery, isRequerying, table, options, ID, allEvents;
+  var createEvent, refresh, settings, clubpicker, clubLabel, club, requery, isRequerying, table, options, ID, allEvents;
   
   ID = "EVENT_OPTIONS";
   options = Data.load(ID, Util.createSet(Data.getUserClubs()));
   
+  
+  settings = (Ti.UI.createButton({
+    title: "Options",
+    style: Ti.UI.iPhone.SystemButtonStyle.BORDERED
+  }));
+  settings.addEventListener('click', function () {
+    Ti.UI.createWindow({
+      url: '/windows/event_options.js',
+      modal: true,
+      onchange: requery
+    }).open();
+  });
+  
+  (function () {
+    var toolbar, space, label;
+    label = Ti.UI.createLabel({
+      text: 'Events for:', color: '#fff'
+    });
+    space = Ti.UI.createButton({ systemButton: Ti.UI.iPhone.SystemButton.FLEXIBLE_SPACE });
+    club = Ti.UI.createButton({ title: 'Club: All', style: Ti.UI.iPhone.SystemButtonStyle.BORDERED });
+    toolbar = Ti.UI.iOS.createToolbar({
+      items: [club, space, settings],
+      bottom: 0, left: 0, right: 0, barColor: Util.theme.darkColor
+    });
+    window.add(toolbar);
+    
+    club.addEventListener('click',function(event){
+      var viewEvent = Ti.UI.createWindow({
+        url: '/windows/pickerWindow.js',
+        opacity: 0
+      });
+      viewEvent.addEventListener('save', function (result) {
+        var value = result.data;
+        club.updateLayout({ title: "Club: " + value });
+        toolbar.items = toolbar.items;
+        Ti.API.log(result);
+        Ti.API.log(value);
+        if (value === "All") {
+        	options = Util.createSet(Data.getUserClubs());
+        } else {
+        	options = Util.createSet([value]);
+        }
+        Ti.API.log(options);
+        requery();
+      });
+      viewEvent.addEventListener('close', function () { viewEvent.close({ opacity: 0, duration: 500 }); });
+      viewEvent.addEventListener('open', function () {
+        var titles = Util.keys(Data.getUserClubs());
+        titles.unshift("All");
+
+        var add = Util.foreach(titles, function (_, title) {
+          return Ti.UI.createPickerRow({ title: title });
+        });
+        viewEvent.fireEvent('data', {
+          data: {
+            type:'club',
+            add:add
+          }
+        });
+        viewEvent.animate({
+          opacity: 1,
+          duration: 500
+        });
+      });
+      viewEvent.open();
+    });
+    
+  })();
+
+  (function () {
+    var clubview, titles, items;
+    titles = Util.keys(Data.getUserClubs());
+    titles.unshift("All");
+    items = Util.foreach(titles, function (_, title) {
+      return Ti.UI.createPickerRow({ title: title });
+    });
+    clubpicker = Ti.UI.createPicker({
+      transform: Ti.UI.create2DMatrix().scale(0.5), width: 680,
+      top: -59, selectionIndicator: true
+    });
+    clubpicker.add(items);
+
+    clubview = Ti.UI.createView({
+      top: 0, height: 88, left: 0, right: 0
+    });
+    clubview.add(clubpicker);
+    window.add(clubview);
+  });
+  
+  
+  
+  
+  
+  
   clubLabel = Ti.UI.createLabel({
   	top:5, left:5, text:'From Club:'
   });
-  window.add(clubLabel);
+  // window.add(clubLabel);
   
   club = Ti.UI.createTextField({
   	top:5, left:100, height: 30, width:130,
   	borderWidth: 1, borderColor: '#bbb', borderRadius: 3 	
   });
-  window.add(club);
+  // window.add(club);
   
   club.addEventListener('focus',function(event){
     var viewEvent = Ti.UI.createWindow({
@@ -43,8 +137,6 @@ Ti.include('/eventHandler.js');
       var add = Util.foreach(titles, function (_, title) {
         return Ti.UI.createPickerRow({ title: title });
       });
-
-
       viewEvent.fireEvent('data', {
         data: {
           type:'club',
@@ -57,9 +149,9 @@ Ti.include('/eventHandler.js');
       });
     });
     viewEvent.open();
-  }); 
+  });
   
-  table = Ti.UI.createTableView({top:35});
+  table = Ti.UI.createTableView({ top: 0, bottom: 41 });
   table.addEventListener('click', function (event) {
     if (!(event.index in allEvents)) return;
     var eventData = allEvents[event.index];
@@ -111,16 +203,12 @@ Ti.include('/eventHandler.js');
     requery();
   });
   
-  settings = (window.leftNavButton = Ti.UI.createButton({
-    title: "Options",
+  refresh = (window.leftNavButton = Ti.UI.createButton({
+    title: "Refresh",
     style: Ti.UI.iPhone.SystemButtonStyle.BORDERED
   }));
-  settings.addEventListener('click', function () {
-    Ti.UI.createWindow({
-      url: '/windows/event_options.js',
-      modal: true,
-      onchange: requery
-    }).open();
+  refresh.addEventListener('click', function () {
+    requery();
   });
 
   // clubsView = createEventsClubs();
